44bd011722a1e92a3524db4697fac9a7fed2cb0d
Reza Tabrizi <63474681+rezabrizi@users.noreply.github.com>
2025-03-27 17:31:04 -0500

    Time: 43 ms (45.23%) Space: 98.6 MB (57.68%)

---
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minimumIndex(vector<int>& nums) {
+
+        pair<int, int> dom = {-1, -1}; 
+        unordered_map<int, int> fq; 
+        int f1 = 0; 
+        
+
+        for (int num: nums){
+            fq[num] ++;
+            if (dom.first < fq[num]){
+                dom = {fq[num], num};
+            }
+        }
+
+        int f2 = dom.first; 
+
+        int n = nums.size(); 
+        for (int i = 0; i < n-1; i++){
+            if (nums[i] == dom.second){
+                f1++; 
+                f2--;
+                if (f1*2 > i+1 && f2*2 > n-i-1 ) return i; 
+            }
+        }
+        return -1;
+        
+    }
+};
