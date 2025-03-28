88835ad92bb8ecbf2398f52d6cb3cc2607d3c02b
Reza Tabrizi <63474681+rezabrizi@users.noreply.github.com>
2025-03-28 09:45:46 -0500

    Time: 349 ms (17.03%) Space: 77.9 MB (19.93%)

--- 2588-maximum-number-of-points-from-grid-queries/maximum-number-of-points-from-grid-queries.cpp
+++ 2588-maximum-number-of-points-from-grid-queries/maximum-number-of-points-from-grid-queries.cpp
@@ -1,88 +1,40 @@
-struct UnionFind {
-    vector<int> rank, parents, size; 
-
-    UnionFind (int n): rank(n, 0), parents(n), size(n, 1){
-        for (int i = 0; i < n; i++) parents[i] = i;
-    }
-
-    int find(int i){
-        if (parents[i] != i){
-            parents[i] = find(parents[i]);
-        }
-        return parents[i]; 
-    }
-
-    bool unite(int x, int y){
-        int parent_x = find(x);
-        int parent_y = find(y);
-        if (parent_x != parent_y){
-            if (rank[parent_x] < rank[parent_y]){
-                parents[parent_x] = parent_y;
-                size[parent_y] += size[parent_x]; 
-            }else{
-                parents[parent_y] = parent_x;
-                size[parent_x] += size[parent_y]; 
-            }
-
-            return false;
-        }
-
-        return true; 
-    }
-    
-    int getSize(int x) {
-        return size[find(x)];
-    }
-};
-
-
 class Solution {
 public:
     vector<int> maxPoints(vector<vector<int>>& grid, vector<int>& queries) {
-        UnionFind uf (grid.size() * grid[0].size());
-        vector<pair<int, int>> queries_idx;
+        priority_queue<pair<int,int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
         for (int i = 0; i < queries.size(); i++){
-            queries_idx.emplace_back(queries[i], i); 
+            pq.push({queries[i], i});
         }
 
-        sort(queries_idx.begin(), queries_idx.end(), [](const auto& a , const auto& b){
-            return a.first < b.first; 
-        }); 
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> cells; 
+        cells.push({grid[0][0], 0, 0});
 
-        vector<vector<int>> grid_vals; 
+        vector<pair<int, int>> dirs = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+        vector<vector<bool>> visited (grid.size(), vector<bool> (grid[0].size())); 
+        visited[0][0] = true; 
+        vector<int> ans (queries.size()); 
+        int curr_size = 0; 
 
-        for (int i = 0; i < grid.size(); i++){
-            for (int j = 0; j < grid[0].size(); j++){
-                grid_vals.push_back({grid[i][j], i, j}); 
-            }
-        }
+        while (!pq.empty()){
+            auto [q, qidx] = pq.top(); pq.pop(); 
 
-        sort(grid_vals.begin(), grid_vals.end());
-
-        vector<int> ans (queries.size());
-        unordered_set<int> visited; 
-        vector<pair<int, int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        int cell_idx = 0; 
-
-        for (const auto& [q, q_idx]: queries_idx){
-            while (cell_idx < grid_vals.size() && grid_vals[cell_idx][0] < q){
-                vector<int> cell = grid_vals[cell_idx++]; 
-                int id = cell[1] * grid[0].size() + cell[2]; 
-                visited.insert(id);
+            while(!cells.empty() && cells.top()[0] < q){
+                vector<int> curr_cell = cells.top(); cells.pop();
+                int x = curr_cell[1], y = curr_cell[2]; 
+                curr_size++; 
 
                 for (const auto& [dx, dy]: dirs){
-                    int nx = cell[1] + dx; 
-                    int ny = cell[2] + dy; 
-                    int nid = nx * grid[0].size() + ny;
-                    if (nx >= 0 && nx < grid.size() && ny >= 0 && ny < grid[0].size() && visited.find(nid) != visited.end()){
-                        uf.unite(id, nid); 
+                    int nx = dx + x, ny = dy + y; 
+                    if (nx >= 0 && nx < grid.size() && ny >=0 && ny < grid[0].size() && visited[nx][ny] == false){
+                        cells.push({grid[nx][ny], nx, ny});
+                        visited[nx][ny] = true;
                     }
                 }
             }
-            ans[q_idx] =  (visited.count(0)) ? uf.getSize(0) : 0; 
-        }
+
+            ans[qidx] = curr_size;
+        }   
         return ans;
     }
-
-
 };
